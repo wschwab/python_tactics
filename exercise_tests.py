56bcff965dfbb5487ed334e4335a44e8bc40800b
wschwab <w.s.schwab4@gmail.com>
2019-08-28 10:33:15 +0300

    added more exercises

--- exercise_tests.py
+++ exercise_tests.py
@@ -4,6 +4,15 @@ import ast
 import inspect
 # needs imports for pytest and watch, I suspect
 
+
+# Helper functions:
+
+# Checks if there is a return in the function passed in as an argument
+def returnsSomething(func):
+    return any(isinstance(node, ast.Return) for node in ast.walk(ast.parse(inspect.getsource(func))))
+
+# Exercise Tests:
+
 # hope this works  -based onhttps://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print/56300627#56300627
 def test_printer(capsys):
     test_case = printer()
@@ -35,9 +44,90 @@ def test_twoStringAdd():
 def test_allStringAdd():
     pass
 
+def test_stringContains():
+    pass
 
-# Helper functions:
+def test_instancesInString():
+    pass
 
-# Checks if there is a return in the function passed in as an argument
-def returnsSomething(func):
-    return any(isinstance(node, ast.Return) for node in ast.walk(ast.parse(inspect.getsource(func))))
+def test_joinWords():
+    pass
+
+def test_censorSentence():
+    pass
+
+def test_addTwo():
+    x = 12049
+    y = 157
+    assert addTwo(x, y) == 12206
+
+def test_addAll():
+    num1,num2,num3,num4 = 1,10,100,1000
+    assert addAll(num1,num2,num3,num4) == 1111
+
+def test_greatest():
+    nums = [1, 10, -100, 50]
+    assert greatest(nums) == 50
+
+def test_biggest():
+    nums = [1, 10, -100, 50]
+    assert biggest(nums) == -100
+
+def test_indexOfGreatest():
+    nums = [1, 10, -100, 50]
+    assert indexOfGreatest(nums) == 3
+
+def test_roundDownToInt():
+    nums = [ 1.5, 10, -10.1233, 5.9 ]
+    expected_output = [1, 10, -10, 5]
+    assert test_roundDownToInt(nums) == expected_output
+
+def test_roundToNearestInt():
+    nums = [ 1.5, 10, -10.1233, 5.9 ]
+    expected_output = [2, 10, -10, 6]
+    assert roundToInt(nums) == expected_output
+
+def test_doubleAll():
+    nums = [1,2,7,10]
+    expected_output = [2,4,14,20]
+    assert doubleAll(nums) == expected_output
+
+def test_piAll():
+    nums = [1,2,7,10]
+    expected_output = [3.141592653589793, 6.283185307179586, 21.991148575128552, 31.41592653589793]
+    assert piAll(nums) == expected_output
+
+def test_piComprehension():
+    # TODO: figure out ast for checking for a comprehension?
+    pass
+
+def test_sliceOfPi():
+    nums = [5,11,3.141592653589793,9]
+    expected_output = [3.141592653589793]
+    assert sliceOfPi(nums) == expected_output
+
+def test_falseOddsComprehension():
+    # TODO: Check for comprehension
+    nums = [1,-8,34,49]
+    expected_output = [False, True, True, False]
+    assert falseOddsComprehension(nums) == expected_output
+
+def test_doubleFirstTwo():
+    # TODO: Check for comprehension
+    nums = [1,-8,34,49]
+    expected_output = [2, -16, 34, 49]
+    assert doubleFirstTwo(nums) == expected_output
+
+def test_formatAsMoney():
+    num1 = 3.5013
+    expected_output1 = '$3.50'
+    num2 = 999
+    expected_output2 = '$999.00'
+    assert formatAsMoney(num1) == expected_output1
+    assert formatAsMoney(num2) == expected_output2
+
+def test_inequality():
+    num1, num2 = 13, 5
+    assert inequality(num1, num2) == '>'
+    assert inequality(num2, num1) == '<'
+    assert inequality(num1, num1) == '='
--- exercises.py
+++ exercises.py
@@ -40,6 +40,146 @@ def capslocker():
 def twoStringAdd():
     pass
 
-# same as beforeu
+# same as before, but the function must add however many strings are passed in
+# example input: 'Life', ' is', ' so', 'wonderful'
+# example output: 'Life is so wonderful'
 def allStringAdd():
+    pass
+
+# this function should take two strings as input
+# and return True when the first contains the second
+# (and False in any other case)
+# example input: 'funkadelic', 'funk'
+# example output: True
+# example input: 'parliment', 'funk'
+# example output: False
+def stringContains():
+    pass
+
+# similar to the above, but this function should return how many times
+# the second string is contained in the first
+# example input: 'blah blah blah monkey', 'blah'
+# example output: 3
+def instancesInString():
+    pass
+
+# HEX AND DECIMAL
+# ask if necessary for Python
+
+# LISTS
+
+# this function receives a list of strings and makes one string out of them
+# example input: (['Writing', 'Python', 'makes', 'me', 'hungry.'])
+# example output: 'Writing Python makes me hungry.'
+def joinWords():
+    pass
+
+# this function should take a sentence (space separated words) and a list of
+# words to censor, and return a new string with the words removed (not replaced
+# with &*#@ or the like)
+# example input: '', [] TODO: Create witty joke here
+# example output: ''
+def censorSentence():
+    pass
+
+# NUMBERS
+
+# take two numbers and add them
+# example input: 12049, 157
+# example output: 12206
+def addTwo():
+    pass
+
+# same as above, but with any number of args
+# example input: 1, 10, 100, 1000
+# example output: 1111
+def addAll():
+    pass
+
+# here we'll get a list of numbers, and need to return the greatest (closest
+# to positive infinity) from them
+# example input: [1, 10, -100, 50]
+# example output: 50
+def greatest():
+    pass
+
+# similar, but here we're looking for the biggest (farthest from 0) number
+# in the list
+# example input: [1, 10, -100, 50]
+# example output: -100
+def biggest():
+    pass
+
+# now instead of the actual number, return the index of the greatest number
+# example input: [1, 10, -100, 50]
+# example output: 3
+def indexOfGreatest():
+    pass
+
+# given a list of floats, round each number down
+# example input: ([ 1.5, 10, -10.1233, 5.9 ])
+# example output: [2, 10, -10, 6] CHECK HOW PYTHON ROUNDS .5
+def roundDownToInt():
+    pass
+
+# similar, but instead of rounding down, wound to the nearest integer
+# example input: ([ 1.5, 10, -10.1233, 5.9 ])
+# example output: [2, 10, -10, 6] CHECK HOW PYTHON ROUNDS .5
+def roundToNearestInt():
+    pass
+
+# given a list of numbers, return a list with every number doubled
+# example input: [1,2,7,10]
+# example output: [2,4,14,20]
+def doubleAll():
+    pass
+
+# same as above, but now return each number in the list times pi
+# example input: [1,2,7,10]
+# example output: [3.141592653589793, 6.283185307179586, 21.991148575128552, 31.41592653589793]
+def piAll():
+    pass
+
+# now do the same in one line of code
+def piComprehension():
+    pass
+
+# given a list of numbers, return a list that only contains pi, if pi is in the original list
+# example input: [5,11,3.141592653589793,9]
+# example output: [3.141592653589793]
+def sliceOfPi():
+    pass
+
+# given a list of numbers, return a list where even numbers are replaced by True
+# and odd numbers are replaced by False in one line of code
+# (0 should return True)
+# example input: [1,-8,34,49]
+# example output: [False, True, True, False]
+def falseOddsComprehension():
+    pass
+
+# given a list of at least three numbers, double ony the first two in one line of code,
+# and return the rest as is
+# example input: [1,-8,34,49]
+# example output: [2, -16, 34, 49]
+def doubleFirstTwo():
+    pass
+
+# given a float or integer, return a string formatted as a price
+# example input: (3.5013)
+# exmaple output '$3.50'
+# example input: (999)
+# example output: '$999.00'
+def formatAsMoney():
+    pass
+
+# given two numbers, return '>' if the first is greater, '<' if the second is greater,
+# and '=' if they are equal
+# example input 1: 13, 5
+# example output 1: >
+# example input 2: 5, 13
+# example output 2: <
+# example input 3: 13, 13
+# example output 3: =
+def inequality():
     pass
